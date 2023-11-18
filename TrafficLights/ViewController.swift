4f5cd30a5da0346e9f3f9550d4039f47eb9bda6e
Matthew <matvey.drozd@gmail.com>
2023-11-18 12:10:28 +0300

    Full creation

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -1,19 +1,56 @@
-//
-//  ViewController.swift
-//  TrafficLights
-//
-//  Created by Дрозд Матвей on 15.11.2023.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var changeLightButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private var lightIsOn = 1.0
+    private var lightIsOff = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        changeLightButton.layer.cornerRadius = 15
+
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.height / 2
+    }
+    
+    @IBAction func changeLightButtonPressed() {
+        if changeLightButton.currentTitle == "Change Light" {
+            changeLightButton.setTitle("Next light", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLightView.alpha = lightIsOff
+            redLightView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLightView.alpha = lightIsOff
+            yellowLightView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLightView.alpha = lightIsOff
+            greenLightView.alpha = lightIsOn
+            currentLight = .red
+        }
+    }
 }
 
+// MARK: CurrentLight
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
